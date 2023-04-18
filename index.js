8dfc783904ffce8bf52bf8862b61e9bf541e3857
Darya <Darsy_13@bk.ru>
2023-04-18 11:13:12 +0300

    add js

---
+++ index.js
@@ -0,0 +1,56 @@
+
+//1. При изменении значения в input с id="from", значение содержащееся в нем должно моментально отображаться в span. 
+//То есть при печати в input'е тег span также должен меняться.
+
+const fromElem = document.getElementById('from');
+const spanElem = document.querySelector('span');
+
+fromElem.addEventListener('input', () => {
+    spanElem.innerText = fromElem.value;
+})
+
+
+
+/*2. При клике на кнопку с классом messageBtn необходимо элементу с классом message:
+- добавить два класса: animate_animated и animate_fadeInLeftBig
+- поставить данному элементу стиль visibility в значение 'visible'.*/
+
+const mesBtn = document.querySelector('.messageBtn');
+const message = document.querySelector('.message');
+mesBtn.addEventListener('click', () => {
+    message.classList.add('animate_animated', 'animate_fadeInLeftBig');
+    message.style.visibility = 'visible';
+})
+
+
+/*3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме. Если какое-либо поле не заполнено, форма не должна отправляться, также должны быть подсвечены незаполненные поля (необходимо поставить класс error незаполненным полям). Как только пользователь начинает заполнять какое-либо поле, необходимо, при вводе в данное поле, произвести проверку:
+- Если поле пустое, необходимо данное поле подсветить (поставить класс error данному полю).
+- Если поле было чем-либо заполнено, подсветку (класс error) необходимо убрать.*/
+
+
+    const firstInput = document.querySelectorAll('form input');
+    const secondInput = document.querySelectorAll('form select');
+    const sendBtn = document.getElementById('send');
+    let controlError = document.getElementsByClassName('.error');
+
+    function error(arr) {
+        arr.forEach(elem => {
+               if (elem.value === "") {
+                    elem.classList.toggle('error');           
+            } else if (elem.classList.contains('error')) {
+                        elem.classList.remove('error');
+                }
+            }
+    )}
+
+    sendBtn.addEventListener('click', () => {
+        error(firstInput);
+        error(secondInput);
+        if (typeof controlError !== "null") {
+            event.preventDefault();
+        }
+    })
+
+    
+        
+    
